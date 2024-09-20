6257e607a08ae37e1339de1dbbe2e795f4091b6d
ROHIT KHARWAR <rohitkumar27699@gmail.com>
2024-09-20 23:12:53 +0530

    some Changes are Done

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
-const { mongoURI } = require('./config');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
-const chatRoutes = require('./routes/chatRoutes');
-const { saveMessage } = require('./controllers/chatController');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const { mongoURI } = require("./config");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const { saveMessage } = require("./controllers/chatController");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/auth', authRoutes);
-app.use('/chats', chatRoutes);
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome<h1>");
+});
 
-mongoose.connect(mongoURI)
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.log('MongoDB connection error:', err));
+app.use("/auth", authRoutes);
+app.use("/chats", chatRoutes);
 
-io.on('connection', (socket) => {
-    console.log('Client connected:', socket.id);
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
-    socket.on('sendMessage', async (messageData) => {
-        const savedMessage = await saveMessage(messageData);
-        io.emit('message', savedMessage); 
-    });
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
 
-    socket.on('disconnect', () => {
-        console.log('Client disconnected:', socket.id);
-    });
+  socket.on("sendMessage", async (messageData) => {
+    const savedMessage = await saveMessage(messageData);
+    io.emit("message", savedMessage);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
 });
 
 const PORT = 4000;
 server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
